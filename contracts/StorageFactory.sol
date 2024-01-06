46590280dfb5ff8e76231fb4179e53eb43d97a89
Haleem-code <abdulhaleemayomide@gmail.com>
2024-01-06 21:57:57 +0100

    learnt how to call a function on simplestorage.sol from simplestoragefactory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.0;
 
 import "./SimpleStorage.sol";
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public{
-        simpleStorage =new SimpleStorage();
+        SimpleStorage simplestorage =new SimpleStorage();
+        simpleStorageArray.push(simplestorage);
     }
+
+    function sfStore(uint256 _simpleStorageIndex,uint256 _simpleStorageNumber) public{
+        SimpleStorage simpleStorage =simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+
+    function sfGet(uint256 _simpleStorageIndex )public view returns(uint256){
+        SimpleStorage simpleStorage =simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retreive();
+    } 
 }
